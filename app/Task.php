e66895c85d03a3f5789471dc4f8515526fc6b35a
emilien letestu <emilien@pc32.home>
2018-07-24 11:13:05 +0200

    [model|Tasks] setters and getters + casts

--- app/Task.php
+++ app/Task.php
@@ -12,4 +12,79 @@ class Task extends Model
     protected $fillable = [
         'name', 'dead_line', 'done', 'done'
     ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'done' => 'boolean'
+    ];
+
+    /**
+     * @param $value
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+    }
+
+    /**
+     * @param $value
+     */
+    public function setDeadLineAttribute($value)
+    {
+        $this->attributes['dead_line'] = $value;
+    }
+
+    /**
+     * @param $value
+     */
+    public function setDescriptionAttrribute($value)
+    {
+        $this->attributes['description'] =  $value;
+    }
+
+    /**
+     * @param $value
+     */
+    public function setDoneAttribute($value)
+    {
+        $this->attributes['done'] = $value;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function getNameAttribute($value)
+    {
+        return $value;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function getDeadLineAttribute($value)
+    {
+        return $value;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function getDescriptionAttribute($value)
+    {
+        return $value;
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public function getDoneAttribute($value)
+    {
+        return $value;
+    }
 }
